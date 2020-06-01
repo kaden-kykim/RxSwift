22ed892c08fec7ad321f41fbd220e04e97a77a6b
kaden-kykim <kaden.kykim@gmail.com>
2020-06-01 14:17:30 -0700

    Add Chapter 7: Transforming Operators

--- Playground/Playground.playground/Pages/Transforming Operators.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Transforming Operators.xcplaygroundpage/Contents.swift
@@ -6,6 +6,109 @@ example(of: "toArray") {
     
     Observable.of("A", "B", "C")
         .toArray()
+        .asObservable()
         .subscribe(onNext: { print($0) })
         .disposed(by: disposeBag)
+}
+
+example(of: "map") {
+    let disposeBag = DisposeBag()
+    
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
+    Observable<Int>.of(123, 4, 56)
+        .map { formatter.string(for: $0) ?? ""}
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+example(of: "enumerated and map") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .enumerated()
+        .map { (index, integer) in index > 2 ? integer * 2 : integer }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+}
+
+struct Student {
+    let score: BehaviorSubject<Int>
+}
+
+example(of: "flatMap") {
+    let disposeBag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 90))
+    
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMap { $0.score }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    student.onNext(laura)
+    laura.score.onNext(85)
+    student.onNext(charlotte)
+    laura.score.onNext(95)
+    charlotte.score.onNext(100)
+}
+
+example(of: "flatMapLatest") {
+    let disposeBag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 90))
+    
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMapLatest { $0.score }
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    student.onNext(laura)
+    laura.score.onNext(85)
+    student.onNext(charlotte)
+    
+    laura.score.onNext(95)
+    charlotte.score.onNext(100)
+}
+
+example(of: "materialize and dematerialize") {
+    
+    enum MyError: Error {
+        case anError
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 100))
+    
+    let student = BehaviorSubject(value: laura)
+    
+    let studentScore = student
+//        .flatMapLatest { $0.score }
+        .flatMapLatest { $0.score.materialize() }
+    studentScore
+        .filter({
+            guard $0.error == nil else {
+                print($0.error!)
+                return false
+            }
+            return true
+        })
+        .dematerialize()
+        .subscribe(onNext: { print($0) })
+        .disposed(by: disposeBag)
+    
+    laura.score.onNext(85)
+    laura.score.onError(MyError.anError)
+    laura.score.onNext(90)
+    
+    student.onNext(charlotte)
 }
