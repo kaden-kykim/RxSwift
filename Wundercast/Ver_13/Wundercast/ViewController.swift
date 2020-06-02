035339e254efef5fffff47d83525562ceaf45043
kaden-kykim <kaden.kykim@gmail.com>
2020-06-01 22:59:26 -0700

    Add 'Showing an activity while searching' part

--- Wundercast/Ver_13/Wundercast/ViewController.swift
+++ Wundercast/Ver_13/Wundercast/ViewController.swift
@@ -32,77 +32,107 @@ import RxCocoa
 import MapKit
 
 class ViewController: UIViewController {
-  @IBOutlet private var mapView: MKMapView!
-  @IBOutlet private var mapButton: UIButton!
-  @IBOutlet private var geoLocationButton: UIButton!
-  @IBOutlet private var activityIndicator: UIActivityIndicatorView!
-  @IBOutlet private var searchCityName: UITextField!
-  @IBOutlet private var tempLabel: UILabel!
-  @IBOutlet private var humidityLabel: UILabel!
-  @IBOutlet private var iconLabel: UILabel!
-  @IBOutlet private var cityNameLabel: UILabel!
-
-  private let bag = DisposeBag()
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-
-    style()
-
-    let search = searchCityName.rx.controlEvent(.editingDidEndOnExit)
-      .map { self.searchCityName.text ?? "" }
-      .filter { !$0.isEmpty }
-      .flatMapLatest { text in
-        return ApiController.shared.currentWeather(city: text)
-          .catchErrorJustReturn(ApiController.Weather.dummy)
-      }
-      .asDriver(onErrorJustReturn: ApiController.Weather.dummy)
-
-    search.map { "\($0.temperature)° C" }
-      .drive(tempLabel.rx.text)
-      .disposed(by: bag)
-
-    search.map { $0.icon }
-      .drive(iconLabel.rx.text)
-      .disposed(by: bag)
-
-    search.map { "\($0.humidity)%" }
-      .drive(humidityLabel.rx.text)
-      .disposed(by: bag)
-
-    search.map { $0.cityName }
-      .drive(cityNameLabel.rx.text)
-      .disposed(by: bag)
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-  }
-
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-
-    Appearance.applyBottomLine(to: searchCityName)
-  }
-
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  // MARK: - Style
-
-  private func style() {
-    view.backgroundColor = UIColor.aztec
-    searchCityName.textColor = UIColor.ufoGreen
-    tempLabel.textColor = UIColor.cream
-    humidityLabel.textColor = UIColor.cream
-    iconLabel.textColor = UIColor.cream
-    cityNameLabel.textColor = UIColor.cream
-  }
+    @IBOutlet private var mapView: MKMapView!
+    @IBOutlet private var mapButton: UIButton!
+    @IBOutlet private var geoLocationButton: UIButton!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var searchCityName: UITextField!
+    @IBOutlet private var tempLabel: UILabel!
+    @IBOutlet private var humidityLabel: UILabel!
+    @IBOutlet private var iconLabel: UILabel!
+    @IBOutlet private var cityNameLabel: UILabel!
+    
+    private let bag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        style()
+        
+        let searchInput = searchCityName.rx.controlEvent(.editingDidEndOnExit)
+            .map { self.searchCityName.text ?? "" }
+            .filter { !$0.isEmpty }
+        
+        let search = searchInput
+            .flatMapLatest ({ text in
+                return ApiController.shared.currentWeather(city: text)
+                    .catchErrorJustReturn(ApiController.Weather.dummy)
+            })
+            .asDriver(onErrorJustReturn: ApiController.Weather.dummy)
+        
+        let running = Observable.merge(
+                searchInput.map { _ in true },
+                search.map { _ in false }.asObservable()
+            )
+            .startWith(true)
+            .asDriver(onErrorJustReturn: false)
+        
+        running
+            .skip(1)
+            .drive(activityIndicator.rx.isAnimating)
+            .disposed(by: bag)
+        
+        running
+            .drive(tempLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        running
+            .drive(iconLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        running
+            .drive(humidityLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        running
+            .drive(cityNameLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        search.map { "\($0.temperature)° C" }
+            .drive(tempLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map { $0.icon }
+            .drive(iconLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map { "\($0.humidity)%" }
+            .drive(humidityLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map { $0.cityName }
+            .drive(cityNameLabel.rx.text)
+            .disposed(by: bag)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        Appearance.applyBottomLine(to: searchCityName)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Style
+    
+    private func style() {
+        view.backgroundColor = UIColor.aztec
+        searchCityName.textColor = UIColor.ufoGreen
+        tempLabel.textColor = UIColor.cream
+        humidityLabel.textColor = UIColor.cream
+        iconLabel.textColor = UIColor.cream
+        cityNameLabel.textColor = UIColor.cream
+    }
 }
