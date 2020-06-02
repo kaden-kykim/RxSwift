47b9a21ca220b5762cdf339f039beda40bc15b9d
kaden-kykim <kaden.kykim@gmail.com>
2020-06-01 21:55:40 -0700

    Commit/Push Test

--- Wundercast/Wundercast/Controllers/ApiController.swift
+++ Wundercast/Wundercast/Controllers/ApiController.swift
@@ -129,7 +129,6 @@ class ApiController {
             urlComponents.queryItems = queryItems
         } else {
             urlComponents.queryItems = [keyQueryItem, unitsQueryItem]
-            
             let jsonData = try! JSONSerialization.data(withJSONObject: params, options: .prettyPrinted)
             request.httpBody = jsonData
         }
