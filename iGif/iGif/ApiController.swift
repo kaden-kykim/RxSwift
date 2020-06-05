f04d476125cd6f9cbcfc22557e462f422b340f14
kaden-kykim <kaden.kykim@gmail.com>
2020-06-04 18:45:33 -0700

    Setup iGif project

--- iGif/iGif/ApiController.swift
+++ iGif/iGif/ApiController.swift
@@ -32,7 +32,7 @@ import RxSwift
 class ApiController {
     static let shared = ApiController()
     
-    private let apiKey = "<#Your Key#>"
+    private let apiKey = giphyAPIKey
     
     func search(text: String) -> Observable<[GiphyGif]> {
         let url = URL(string: "http://api.giphy.com/v1/gifs/search")!
